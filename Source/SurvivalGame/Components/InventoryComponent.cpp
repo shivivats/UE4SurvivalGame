ec7cee56cffd66da3468917ace59947444d38ac4
Shivi <shivivats7@gmail.com>
2021-04-14 17:16:04 +0200

    halfway through ep 13. pausing here for now as the course is way too fast and way too much for me

--- Source/SurvivalGame/Components/InventoryComponent.cpp
+++ Source/SurvivalGame/Components/InventoryComponent.cpp
@@ -6,11 +6,9 @@
 // Sets default values for this component's properties
 UInventoryComponent::UInventoryComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	SetIsReplicated(true);
 
-	// ...
+
 }
 
 
@@ -30,5 +28,10 @@ void UInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FA
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+void UInventoryComponent::OnRep_Items()
+{
+
 }
 
--- Source/SurvivalGame/Components/InventoryComponent.h
+++ Source/SurvivalGame/Components/InventoryComponent.h
@@ -24,5 +24,26 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+protected:
+
+	/** The maximum weight the inventory can hold, For players, backpacks and other items increase this limit */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory")
+	float WeightCapacity;
+
+	/** The maximum number of items the inventory can hold. For players, backpacks and other items increase this limit */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory", meta = (ClampMin = 0, ClampMax = 200))
+	int32 Capacity;
+
+	/** Array of items that the inventory has */
+	UPROPERTY(ReplicatedUsing = OnRep_Items, VisibleAnywhere, Category = "Inventory")
+	TArray<class UItem*> Items;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual bool ReplicateSubobjects(class UActorChannel* Channel, class FOutBunch* Bunch, FReplicationFlags* RepFlags) override;
+
+private:
+
+	UFUNCTION()
+	void OnRep_Items();
+
 };
